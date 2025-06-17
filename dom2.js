7499765c07f26854867346d2e38b0ca4779e305a
Serhii Dydunets <sergijdudinec69@gmail.com>
2025-06-17 21:47:24 +0300

    add

---
+++ dom2.js
@@ -0,0 +1,19 @@
+// const red = document.querySelector('.red');
+// const blue = document.querySelector('.blue');
+// const green = document.querySelector('.green');
+// red.addEventListener('change', () => {
+//     document.body.style.backgroundColor = 'red';
+
+// })
+// blue.addEventListener('change', () => {
+//     document.body.style.backgroundColor = 'blue';
+// });
+
+// green.addEventListener('change', () => {
+//     document.body.style.backgroundColor = 'green';
+// });
+const inpu = document.querySelector('#name-input');
+const input = document.querySelector('#name-output');
+inpu.addEventListener('sumbit', () => {
+    name-output.
+})
